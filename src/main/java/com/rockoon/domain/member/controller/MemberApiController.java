285bbba5c0529e1874bb0315d24489072bc88d1a
hann <wjdgks1233@gmail.com>
2024-05-16 15:27:31 +0900

    [Feat/#16] refactor controller test code

--- src/main/java/com/rockoon/domain/member/controller/MemberApiController.java
+++ src/main/java/com/rockoon/domain/member/controller/MemberApiController.java
@@ -5,6 +5,7 @@ import com.rockoon.domain.member.dto.MemberRequest.MemberRegisterDto;
 import com.rockoon.domain.member.entity.Member;
 import com.rockoon.domain.member.service.MemberCommandService;
 import com.rockoon.domain.member.service.MemberQueryService;
+import com.rockoon.presentation.payload.dto.ApiResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,20 +17,19 @@ public class MemberApiController {
     private final MemberQueryService memberQueryService;
 
     @PostMapping
-    public void registerMember(@RequestBody MemberRegisterDto memberRegisterDto) {
-        //TODO return memberId
-        memberCommandService.registerMember(memberRegisterDto);
+    public ApiResponseDto<Long> registerMember(@RequestBody MemberRegisterDto memberRegisterDto) {
+        return ApiResponseDto.onSuccess(memberCommandService.registerMember(memberRegisterDto));
     }
 
     @PutMapping("/{memberId}")          //TODO path -> memberId 삭제, jwt token을 사용해서 member 바로 조회
-    public void modifyMemberInfo(@PathVariable Long memberId,
+    public ApiResponseDto<Long> modifyMemberInfo(@PathVariable Long memberId,
                                  @RequestBody MemberRequest.MemberModifyDto memberModifyDto) {
         Member member = memberQueryService.getByMemberId(memberId);
-        memberCommandService.modifyMemberInfo(member, memberModifyDto);
+        return ApiResponseDto.onSuccess(memberCommandService.modifyMemberInfo(member, memberModifyDto));
     }
 
     @GetMapping("/{memberId}")
-    public Member getMemberInfo(@PathVariable Long memberId) {
-        return memberQueryService.getByMemberId(memberId);
+    public ApiResponseDto<Member> getMemberInfo(@PathVariable Long memberId) {
+        return ApiResponseDto.onSuccess(memberQueryService.getByMemberId(memberId));
     }
 }
