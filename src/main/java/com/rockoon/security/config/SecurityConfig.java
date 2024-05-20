2d6bff46e8d0f689bbfa26a833b4b1477c2e2349
hann <wjdgks1233@gmail.com>
2024-05-20 15:05:50 +0900

    [Feat/#20] set init of securityConfig

--- src/main/java/com/rockoon/security/config/SecurityConfig.java
+++ src/main/java/com/rockoon/security/config/SecurityConfig.java
@@ -1,4 +1,62 @@
 package com.rockoon.security.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        configureCorsAndSecurity(httpSecurity);
+        configureAuth(httpSecurity);
+
+        return httpSecurity.build();
+    }
+
+    private static void configureCorsAndSecurity(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .headers(
+                        httpSecurityHeadersConfigurer ->
+                                httpSecurityHeadersConfigurer.frameOptions(
+                                        HeadersConfigurer.FrameOptionsConfig::disable
+                                )
+                )
+                // stateless한 rest api 이므로 csrf 공격 옵션 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(HttpBasicConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(configurer -> configurer
+                        .sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                );
+    }
+
+    private void configureAuth(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizeRequest -> {
+                    authorizeRequest
+                            .requestMatchers("/ws/**", "/subscribe/**", "/publish/**").permitAll()
+                            .requestMatchers("/", "/.well-known/**", "/css/**", "/*.ico", "/error", "/images/**").permitAll()
+                            .anyRequest().permitAll();
+//                            .requestMatchers(authorizationAdmin()).hasRole("ADMIN")
+//                            .requestMatchers(authorizationDormant()).hasRole("DORMANT")
+//                            .requestMatchers(authorizationGuest()).hasRole("GUEST")
+//                            .requestMatchers(authorizationUser()).hasRole("USER");
+                });
+    }
 }
