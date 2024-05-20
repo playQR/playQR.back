212e7826c8aa2e5d8d68e1cd24cb52bb7fdb846b
hann <wjdgks1233@gmail.com>
2024-05-20 17:36:57 +0900

    [Feat/#20] make authUser annotation

---
+++ src/main/java/com/rockoon/global/annotation/auth/CustomAuthenticationPrincipalArgumentResolver.java
@@ -0,0 +1,70 @@
+package com.rockoon.global.annotation.auth;
+
+
+import com.rockoon.domain.member.entity.Member;
+import com.rockoon.domain.member.service.MemberQueryService;
+import com.rockoon.presentation.payload.code.ErrorStatus;
+import com.rockoon.presentation.payload.exception.SecurityHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.lang.annotation.Annotation;
+
+@Slf4j
+@RequiredArgsConstructor
+public final class CustomAuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberQueryService memberQueryService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return findMethodAnnotation(AuthUser.class, parameter) != null;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Object principal = authentication.getPrincipal();
+        AuthUser annotation = findMethodAnnotation(AuthUser.class, parameter);
+        if (principal == "anonymousUser") {
+            throw new SecurityHandler(ErrorStatus._UNAUTHORIZED_LOGIN_DATA_RETRIEVAL_ERROR);
+        }
+        findMethodAnnotation(AuthUser.class, parameter);
+        Member member = memberQueryService.getMemberByUsername(authentication.getName());
+
+        if (principal != null && !ClassUtils.isAssignable(parameter.getParameterType(), member.getClass())) {
+            if (annotation.errorOnInvalidType()) {
+                throw new SecurityHandler(ErrorStatus._ASSIGNABLE_PARAMETER);
+            }
+        }
+
+        return member;
+    }
+
+    private <T extends Annotation> T findMethodAnnotation(Class<T> annotationClass, MethodParameter parameter) {
+        T annotation = parameter.getParameterAnnotation(annotationClass);
+        if (annotation != null) {
+            return annotation;
+        }
+        Annotation[] annotationsToSearch = parameter.getParameterAnnotations();
+        for (Annotation toSearch : annotationsToSearch) {
+            annotation = AnnotationUtils.findAnnotation(toSearch.annotationType(), annotationClass);
+            if (annotation != null) {
+                return annotation;
+            }
+        }
+        return null;
+    }
+}
