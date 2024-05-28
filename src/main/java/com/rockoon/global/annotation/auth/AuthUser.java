b5dad5dc7159a12423e8004ef295dd6dda4268c5
hann <wjdgks1233@gmail.com>
2024-05-28 17:25:22 +0900

    [Feat/#24] refactor authuser annotation for not showing(hidden option on)

--- src/main/java/com/rockoon/global/annotation/auth/AuthUser.java
+++ src/main/java/com/rockoon/global/annotation/auth/AuthUser.java
@@ -1,10 +1,12 @@
 package com.rockoon.global.annotation.auth;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.*;
 
 @Target({ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-//@Parameter(hidden=true)      TODO swagger 사용 시 설정 해주기
+@Parameter(hidden=true)
 @Documented
 public @interface AuthUser {
     boolean errorOnInvalidType() default true;
