cbc2bd21c64d02c3d86aa2edc015fa57b33ba8af
k168ww <k168ww@gmail.com>
2024-06-18 16:08:41 +0900

    #35

--- src/main/java/com/rockoon/domain/ticket/controller/GuestController.java
+++ src/main/java/com/rockoon/domain/ticket/controller/GuestController.java
@@ -15,28 +15,28 @@ public class GuestController {
     @Autowired
     private GuestService guestService;
 
-    // Guest를 업데이트하는 메소드
+    // Guest를 업데이트
     @PutMapping("/{guestId}")
     public ResponseEntity<Guest> updateGuest(@PathVariable Long guestId, @RequestBody Guest guestDetails) {
         Guest updatedGuest = guestService.updateGuest(guestId, guestDetails);
         return ResponseEntity.ok(updatedGuest);
     }
 
-    // 티켓을 발행하는 메소드
+    // 티켓 발행
     @PostMapping("/issue-ticket")
     public ResponseEntity<Guest> issueTicket(@RequestParam Long guestId) {
         Guest updatedGuest = guestService.issueTicket(guestId);
         return ResponseEntity.ok(updatedGuest);
     }
 
-    // 특정 프로모션의 모든 게스트를 가져오는 메소드
+    // 특정 프로모션의 모든 게스트를 가져오기
     @GetMapping("/promotions/{promotionId}")
     public ResponseEntity<List<Guest>> getGuestsByPromotion(@PathVariable Long promotionId) {
         List<Guest> guests = guestService.findGuestsByPromotionId(promotionId);
         return ResponseEntity.ok(guests);
     }
 
-    // 특정 게스트를 삭제하는 메소드
+    // 특정 게스트를 삭제
     @DeleteMapping("/{guestId}")
     public ResponseEntity<Void> deleteGuest(@PathVariable Long guestId) {
         guestService.deleteGuest(guestId);
