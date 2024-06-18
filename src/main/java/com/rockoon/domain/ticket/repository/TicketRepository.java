06fa7a9b63c4677a6f7c03fbcb92702b85b83a0f
k168ww <k168ww@gmail.com>
2024-06-18 21:03:33 +0900

    #35 Ticket 관련 수정

--- src/main/java/com/rockoon/domain/ticket/repository/TicketRepository.java
+++ src/main/java/com/rockoon/domain/ticket/repository/TicketRepository.java
@@ -4,6 +4,11 @@ import com.rockoon.domain.ticket.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    // Guest ID로 티켓 조회
+    Optional<Ticket> findByGuestId(Long guestId);
 }
--- src/main/java/com/rockoon/domain/ticket/service/TicketService.java
+++ src/main/java/com/rockoon/domain/ticket/service/TicketService.java
@@ -1,16 +1,23 @@
 package com.rockoon.domain.ticket.service;
 
 import com.rockoon.domain.ticket.entity.Ticket;
-import com.rockoon.domain.ticket.repository.TicketRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class TicketService {
+import java.util.List;
 
-    @Autowired
-    private TicketRepository ticketRepository;
+public interface TicketService {
 
+    // 티켓 생성 및 발급
+    Ticket issueTicket(Long guestId);
 
+    // 모든 티켓 조회
+    List<Ticket> findAllTickets();
 
+    // 티켓 ID로 티켓 조회
+    Ticket findTicketById(Long id);
+
+    // 게스트 ID로 티켓 조회
+    Ticket findTicketByGuestId(Long guestId);
+
+    // 티켓 삭제
+    void deleteTicket(Long id);
 }
