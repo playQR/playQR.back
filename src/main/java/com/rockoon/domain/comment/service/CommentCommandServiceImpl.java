d95b55e8bb41383545ff8344de6faf429bd1271a
hann <wjdgks1233@gmail.com>
2024-06-18 15:03:52 +0900

    [Feat/comment/#40] create comment business logic

--- src/main/java/com/rockoon/domain/comment/service/CommentCommandServiceImpl.java
+++ src/main/java/com/rockoon/domain/comment/service/CommentCommandServiceImpl.java
@@ -1,4 +1,32 @@
 package com.rockoon.domain.comment.service;
 
-public class CommentCommandServiceImpl {
+import com.rockoon.domain.board.entity.Promotion;
+import com.rockoon.domain.board.repository.PromotionRepository;
+import com.rockoon.domain.comment.entity.Comment;
+import com.rockoon.domain.comment.repository.CommentRepository;
+import com.rockoon.domain.member.entity.Member;
+import com.rockoon.presentation.payload.code.ErrorStatus;
+import com.rockoon.presentation.payload.exception.PromotionHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CommentCommandServiceImpl implements CommentCommandService{
+
+    private final CommentRepository commentRepository;
+    private final PromotionRepository promotionRepository;
+
+    @Override
+    public Long createComment(Member member, Long promotionId, CommentRequest request) {
+        Promotion promotion = promotionRepository.findById(promotionId)
+                .orElseThrow(() -> new PromotionHandler(ErrorStatus.PROMOTION_NOT_FOUND));
+        Comment comment = commentRepository.save(Comment.of(member, promotion, request));
+        return comment.getId();
+    }
+
+    @Override
+    public void removeComment(Member member, Long commentId) {
+
+    }
 }
