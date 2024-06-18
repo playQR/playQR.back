c4b997cdd93acac0663380b836ade517978dad26
hann <wjdgks1233@gmail.com>
2024-06-18 16:32:41 +0900

    [Feat/comment/#40] create get pagination comment list

--- src/main/java/com/rockoon/domain/comment/service/CommentQueryService.java
+++ src/main/java/com/rockoon/domain/comment/service/CommentQueryService.java
@@ -1,4 +1,11 @@
 package com.rockoon.domain.comment.service;
 
+import com.rockoon.domain.comment.entity.Comment;
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
+
 public interface CommentQueryService {
+
+    Page<Comment> getPaginationCommentByPromotionId(Long promotionId, Pageable pageable);
 }
--- src/main/java/com/rockoon/domain/comment/service/CommentQueryServiceImpl.java
+++ src/main/java/com/rockoon/domain/comment/service/CommentQueryServiceImpl.java
@@ -1,9 +1,20 @@
 package com.rockoon.domain.comment.service;
 
+import com.rockoon.domain.comment.entity.Comment;
+import com.rockoon.domain.comment.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
 
 @RequiredArgsConstructor
 @Service
 public class CommentQueryServiceImpl implements CommentQueryService{
+    private final CommentRepository commentRepository;
+    @Override
+    public Page<Comment> getPaginationCommentByPromotionId(Long promotionId, Pageable pageable) {
+        commentRepository.findByPromotionId(promotionId, pageable);
+        return null;
+    }
 }
