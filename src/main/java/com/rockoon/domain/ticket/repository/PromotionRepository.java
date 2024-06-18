6ea5e4279eaea6acc2ca3a85c8bf9d033e62a2ea
k168ww <k168ww@gmail.com>
2024-06-18 15:52:26 +0900

    #35 Guest 관련 코드 수정

--- src/main/java/com/rockoon/domain/ticket/repository/PromotionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rockoon.domain.ticket.repository;
-
-import com.rockoon.domain.board.entity.Promotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-}
