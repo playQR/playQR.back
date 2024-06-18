f551da167ee122140c60b50b8e8f7421ea37c627
k168ww <k168ww@gmail.com>
2024-06-18 23:12:33 +0900

    #35 data 난수로

--- src/main/java/com/rockoon/domain/ticket/service/guest/GuestCommandService.java
+++ src/main/java/com/rockoon/domain/ticket/service/guest/GuestCommandService.java
@@ -5,12 +5,9 @@ import com.rockoon.domain.member.entity.Member;
 
 public interface GuestCommandService {
 
-    // 게스트 생성
     Guest createGuest(Long promotionId, Member member, String name);
 
-    // 게스트 정보 수정
     Guest updateGuest(Long guestId, Guest guestDetails);
 
-    // 게스트 삭제
     void deleteGuest(Long id);
 }
