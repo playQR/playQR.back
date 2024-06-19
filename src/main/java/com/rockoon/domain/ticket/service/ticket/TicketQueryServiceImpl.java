967b34006dd20d4d9b632e9a4801aaf5cda20c4a
k168ww <k168ww@gmail.com>
2024-06-19 13:10:30 +0900

    #35 TicketQueryService

--- src/main/java/com/rockoon/domain/ticket/service/ticket/TicketQueryServiceImpl.java
+++ src/main/java/com/rockoon/domain/ticket/service/ticket/TicketQueryServiceImpl.java
@@ -1,4 +1,29 @@
 package com.rockoon.domain.ticket.service.ticket;
 
-public class TicketQueryServiceImpl {
+import com.rockoon.domain.ticket.entity.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class TicketQueryServiceImpl implements TicketQueryService {
+    @Override
+    public List<Ticket> findAllTickets() {
+        return List.of();
+    }
+
+    @Override
+    public Ticket findTicketById(Long ticketId) {
+        return null;
+    }
+
+    @Override
+    public Ticket findTicketByGuestId(Long guestId) {
+        return null;
+    }
+
+    @Override
+    public Page<Ticket> findTickets(Pageable pageable) {
+        return null;
+    }
 }
