53a1affde3caf9955722a366f270eef44013c71a
k168ww <k168ww@gmail.com>
2024-06-19 18:46:04 +0900

    #35 GuestQueryService 비즈니스 로직

--- src/main/java/com/rockoon/domain/ticket/service/guest/GuestQueryServiceImpl.java
+++ src/main/java/com/rockoon/domain/ticket/service/guest/GuestQueryServiceImpl.java
@@ -1,29 +1,41 @@
 package com.rockoon.domain.ticket.service.guest;
 
 import com.rockoon.domain.ticket.entity.Guest;
+import com.rockoon.domain.ticket.repository.GuestRepository;
+import com.rockoon.global.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
 public class GuestQueryServiceImpl implements GuestQueryService{
+
+    private final GuestRepository guestRepository;
+
     @Override
     public List<Guest> findAllGuests() {
-        return List.of();
+        return guestRepository.findAll();
     }
 
     @Override
     public Guest findGuestById(Long guestId) {
-        return null;
+        return guestRepository.findById(guestId)
+                .orElseThrow(() -> new ResourceNotFoundException("Guest not found for id: " + guestId));
     }
 
     @Override
     public List<Guest> findGuestsByPromotionId(Long promotionId) {
-        return List.of();
+        return guestRepository.findByPromotionId(promotionId);
     }
 
     @Override
     public Page<Guest> findGuestsByPromotionId(Long promotionId, Pageable pageable) {
-        return null;
+        return guestRepository.findByPromotionId(promotionId, pageable);
     }
 }
