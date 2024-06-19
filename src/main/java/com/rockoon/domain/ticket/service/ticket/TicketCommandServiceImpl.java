6a57467a318fe39318efeafcc1bebc18be694b87
k168ww <k168ww@gmail.com>
2024-06-19 13:10:30 +0900

    #35 TicketQueryService

--- src/main/java/com/rockoon/domain/ticket/service/ticket/TicketCommandServiceImpl.java
+++ src/main/java/com/rockoon/domain/ticket/service/ticket/TicketCommandServiceImpl.java
@@ -5,7 +5,7 @@ import com.rockoon.domain.member.entity.Member;
 public class TicketCommandServiceImpl implements TicketCommandService {
     @Override
     public Long issueTicket(Long guestId, Member member) {
-        return 0;
+        return 0L;
     }
 
     @Override
