4f382eaba604496417b434c4fc7cd9f9e746435b
k168ww <k168ww@gmail.com>
2024-06-19 13:10:30 +0900

    #35 TicketQueryService

--- src/main/java/com/rockoon/domain/ticket/service/ticket/TicketQueryService.java
+++ src/main/java/com/rockoon/domain/ticket/service/ticket/TicketQueryService.java
@@ -1,4 +1,18 @@
 package com.rockoon.domain.ticket.service.ticket;
 
+import com.rockoon.domain.ticket.entity.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface TicketQueryService {
+
+    List<Ticket> findAllTickets();
+
+    Ticket findTicketById(Long ticketId);
+
+    Ticket findTicketByGuestId(Long guestId);
+
+    Page<Ticket> findTickets(Pageable pageable);
 }
