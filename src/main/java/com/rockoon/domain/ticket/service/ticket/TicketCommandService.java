3cb98b10fc38b6c1a545842d27cc31da483033b7
k168ww <k168ww@gmail.com>
2024-06-19 13:09:27 +0900

    #35 TicketCommandService

--- src/main/java/com/rockoon/domain/ticket/service/ticket/TicketCommandService.java
+++ src/main/java/com/rockoon/domain/ticket/service/ticket/TicketCommandService.java
@@ -1,4 +1,10 @@
 package com.rockoon.domain.ticket.service.ticket;
 
+import com.rockoon.domain.member.entity.Member;
+
 public interface TicketCommandService {
+
+    Long issueTicket(Long guestId, Member member);
+
+    void deleteTicket(Long ticketId, Member member);
 }
