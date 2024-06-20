774a8685c9a595864c5c75894d7a1d11f666c01b
k168ww <k168ww@gmail.com>
2024-06-20 14:12:02 +0900

    #35 TicketQueryService 비즈니스 로직

--- src/main/java/com/rockoon/domain/ticket/service/ticket/TicketQueryServiceImpl.java
+++ src/main/java/com/rockoon/domain/ticket/service/ticket/TicketQueryServiceImpl.java
@@ -1,29 +1,41 @@
 package com.rockoon.domain.ticket.service.ticket;
 
 import com.rockoon.domain.ticket.entity.Ticket;
+import com.rockoon.domain.ticket.repository.TicketRepository;
+import com.rockoon.global.exception.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class TicketQueryServiceImpl implements TicketQueryService {
-    @Override
-    public List<Ticket> findAllTickets() {
-        return List.of();
-    }
+
+    private final TicketRepository ticketRepository;
 
     @Override
     public Ticket findTicketById(Long ticketId) {
-        return null;
+        return ticketRepository.findById(ticketId)
+                .orElseThrow(() -> new ResourceNotFoundException("Ticket not found for id: " + ticketId));
     }
 
     @Override
-    public Ticket findTicketByGuestId(Long guestId) {
-        return null;
+    public List<Ticket> findAllTickets() {
+        return ticketRepository.findAll();
     }
 
     @Override
-    public Page<Ticket> findTickets(Pageable pageable) {
-        return null;
+    public Page<Ticket> findTicketsByGuestId(Long guestId, Pageable pageable) {
+        return ticketRepository.findByGuestId(guestId, pageable);
+    }
+
+    @Override
+    public List<Ticket> findTicketsByPromotionId(Long promotionId) {
+        return ticketRepository.findByPromotionId(promotionId);
     }
 }
