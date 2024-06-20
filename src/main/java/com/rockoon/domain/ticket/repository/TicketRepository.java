86e6bdefff5f70258c186dd794f71a97957bd466
k168ww <k168ww@gmail.com>
2024-06-20 14:12:02 +0900

    #35 TicketQueryService 비즈니스 로직

--- src/main/java/com/rockoon/domain/ticket/repository/TicketRepository.java
+++ src/main/java/com/rockoon/domain/ticket/repository/TicketRepository.java
@@ -1,9 +1,12 @@
 package com.rockoon.domain.ticket.repository;
 
 import com.rockoon.domain.ticket.entity.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,7 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     // Guest ID로 티켓 조회
     Optional<Ticket> findByGuestId(Long guestId);
+
+    Page<Ticket> findByGuestId(Long guestId, Pageable pageable);
+    List<Ticket> findByPromotionId(Long promotionId);
 }
