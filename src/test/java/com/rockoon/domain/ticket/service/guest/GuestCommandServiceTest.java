397318315f98b1dbc9730c70298fdde3a16e711f
hann <wjdgks1233@gmail.com>
2024-06-21 10:35:47 +0900

    [Test/Guest/#42] create test code of guest command service

--- src/test/java/com/rockoon/domain/ticket/service/guest/GuestCommandServiceTest.java
+++ src/test/java/com/rockoon/domain/ticket/service/guest/GuestCommandServiceTest.java
@@ -9,6 +9,9 @@ import com.rockoon.domain.member.service.MemberQueryService;
 import com.rockoon.domain.ticket.entity.Guest;
 import com.rockoon.domain.ticket.repository.GuestRepository;
 import com.rockoon.global.config.test.DatabaseCleanUp;
+import com.rockoon.presentation.payload.code.ErrorStatus;
+import com.rockoon.presentation.payload.exception.GuestHandler;
+import com.rockoon.presentation.payload.exception.PromotionHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
@@ -71,7 +75,7 @@ class GuestCommandServiceTest {
 
     @Test
     @Transactional
-    @DisplayName("유저가 프로모션의 게스트를 신청합니다. 신청된 게스트의 상태는 티켓 x, 입장 x상태가 디폴트입니다.")
+    @DisplayName("유저가 프로모션의 게스트를 신청합니다. 신청된 게스트의 상태는 티켓 false, 입장 false 상태가 디폴트입니다.")
     void createGuest() {
         //given
         PromotionRequest request = PromotionRequest.builder()
@@ -89,6 +93,66 @@ class GuestCommandServiceTest {
         assertThat(byId.get().getEntered()).isFalse();
         assertThat(byId.get().getTicketIssued()).isFalse();
         assertThat(byId.get().getName()).isEqualTo("guestName");
+    }
+    @Test
+    @DisplayName("유저가 프로모션의 게스트 신청 시, 프로모션이 없는 경우 예외를 발생시킵니다.")
+    void executeExceptionNotFoundPromotion() {
+        //given
+        Long proxyId = 1000L;
+        //when & then
+        assertThatThrownBy(() -> guestCommandService.createGuest(proxyId, host, "guestName"))
+                .isInstanceOf(PromotionHandler.class)
+                .hasMessage(ErrorStatus.PROMOTION_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("게스트 정보를 수정합니다. 변경 값은 게스트 이름입니다.")
+    void updateGuest() {
+        //given
+        PromotionRequest request = PromotionRequest.builder()
+                .title("title")
+                .team("team")
+                .content("content")
+                .maxAudience(3)
+                .build();
+        String convertedName = "convertedName";
+        Long promotionId = promotionCommandService.createPromotion(host, request);
+        Long guestId = guestCommandService.createGuest(promotionId, guest, "guestName");
+        //when
+        guestCommandService.updateGuest(guestId, guest,  convertedName);   //TODO edit argument
+        //then
+        Guest updatedGuest = guestRepository.findById(guestId).get();
+        assertThat(updatedGuest.getName()).isEqualTo(convertedName);
+    }
+    @Test
+    @DisplayName("게스트 정보를 수정할 때, 신청자와 수정 시도 유저의 정보가 다른 경우 예외를 발생시킵니다.")
+    void executeExceptionNotValidateCreator() {
+        //given
+        PromotionRequest request = PromotionRequest.builder()
+                .title("title")
+                .team("team")
+                .content("content")
+                .maxAudience(3)
+                .build();
+        Member notGuest = Member.builder().build();
+        Long promotionId = promotionCommandService.createPromotion(host, request);
+        Long guestId = guestCommandService.createGuest(promotionId, guest, "guestName");
+        //when & then
+        assertThatThrownBy(() -> guestCommandService.updateGuest(guestId, notGuest, "exception"))
+                .isInstanceOf(GuestHandler.class)
+                .hasMessage(ErrorStatus.GUEST_ONLY_CAN_BE_TOUCHED_BY_CREATOR.getMessage());
+    }
+
+    @Test
+    @DisplayName("게스트 정보를 수정할 때, 게스트의 정보가 없을 경우 예외를 발생시킵니다.")
+    void executeExceptionNotFoundGuest() {
+        //given
+        Long proxyId = 1000L;
+        //when & then
+        assertThatThrownBy(() -> guestCommandService.updateGuest(proxyId, guest, "exception"))
+                .isInstanceOf(GuestHandler.class)
+                .hasMessage(ErrorStatus.GUEST_NOT_FOUND.getMessage());
     }
 
 }
