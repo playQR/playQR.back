b07d2aa54d5490f4864499626b6e2f1d17031790
hann <wjdgks1233@gmail.com>
2024-06-21 17:21:59 +0900

    [Feat/Bucket/#37] apply in member

--- src/main/java/com/rockoon/domain/member/service/MemberCommandServiceImpl.java
+++ src/main/java/com/rockoon/domain/member/service/MemberCommandServiceImpl.java
@@ -5,6 +5,8 @@ import com.rockoon.domain.member.dto.MemberRequest.MemberRegisterDto;
 import com.rockoon.domain.member.entity.Member;
 import com.rockoon.domain.member.entity.Role;
 import com.rockoon.domain.member.repository.MemberRepository;
+import com.rockoon.global.service.AwsS3Service;
+import com.rockoon.global.util.ImageUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +18,7 @@ import java.util.UUID;
 @Service
 public class MemberCommandServiceImpl implements MemberCommandService {
     private final MemberRepository memberRepository;
+    private final AwsS3Service awsS3Service;
     @Override
     public Long registerMember(MemberRegisterDto memberRequest) {
         Member member = Member.of(memberRequest);
@@ -26,6 +29,9 @@ public class MemberCommandServiceImpl implements MemberCommandService {
 
     @Override
     public Long modifyMemberInfo(Member member, MemberModifyDto memberRequest) {
+        if (ImageUtil.validateRemoveImgInS3(member.getProfileImg(), memberRequest.getProfileImg())) {
+            awsS3Service.deleteFile(member.getProfileImg());
+        }
         member.modifyInfo(memberRequest);
         return member.getId();
     }
