d6e3b2d3e85c84c119a3555c80d7cc1661639252
k168ww <k168ww@gmail.com>
2024-06-23 00:23:11 +0900

    #35 피드백 반영

--- src/main/java/com/rockoon/domain/ticket/repository/TicketRepository.java
+++ src/main/java/com/rockoon/domain/ticket/repository/TicketRepository.java
@@ -1,9 +1,11 @@
 package com.rockoon.domain.ticket.repository;
 
 import com.rockoon.domain.ticket.entity.Ticket;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,11 +15,12 @@ import java.util.UUID;
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-    Optional<Ticket> findByUuid(UUID uuid);
+    Optional<Ticket> findByUuid(String uuid);
 
     Optional<Ticket> findByGuestId(Long guestId);
 
     Page<Ticket> findByGuestId(Long guestId, Pageable pageable);
 
-    List<Ticket> findByPromotionId(Long promotionId);
+    @Query("SELECT t FROM Ticket t WHERE t.guest.promotion.id = :promotionId")
+    List<Ticket> findByPromotionId(@Param("promotionId") Long promotionId);
 }
