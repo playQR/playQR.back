3a0b9f88b871ddff308913b2bdd327b3fc801ffc
hann <wjdgks1233@gmail.com>
2024-06-23 08:18:23 +0900

    [Feat/Like/#38] create like music query service

--- src/main/java/com/rockoon/domain/like/service/music_like/LikeMusicQueryService.java
+++ src/main/java/com/rockoon/domain/like/service/music_like/LikeMusicQueryService.java
@@ -1,4 +1,10 @@
 package com.rockoon.domain.like.service.music_like;
 
+import com.rockoon.domain.member.entity.Member;
+
 public interface LikeMusicQueryService {
+
+    boolean isLiked(Long promotionMusicId, Member member);
+
+    long countLike(Long promotionMusicId);
 }
