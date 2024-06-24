3c56b7ad8983504f2ef527d7f9a8d7c750d306f5
hann <wjdgks1233@gmail.com>
2024-06-24 16:04:28 +0900

    [Feat/Like/#38] create MusicConvereter for response dto of promotion

--- src/main/java/com/rockoon/domain/board/converter/PromotionConverter.java
+++ src/main/java/com/rockoon/domain/board/converter/PromotionConverter.java
@@ -5,6 +5,7 @@ import com.rockoon.domain.board.dto.promotion.PromotionResponse.PromotionListDto
 import com.rockoon.domain.board.dto.promotion.PromotionResponse.PromotionSummaryDto;
 import com.rockoon.domain.board.entity.Promotion;
 import com.rockoon.domain.member.converter.MemberConverter;
+import com.rockoon.domain.music.converter.MusicConverter;
 import com.rockoon.global.util.ImageUtil;
 import org.springframework.data.domain.Page;
 
@@ -19,6 +20,8 @@ public class PromotionConverter {
                 .content(promotion.getContent())
                 .maxAudience(promotion.getMaxAudience())
                 .writer(MemberConverter.toResponse(promotion.getWriter()))
+                .musicList(promotion.getPromotionMusicList().stream()
+                        .map(MusicConverter::toViewDto).collect(Collectors.toList()))
                 .imageList(promotion.getBoardImageList().stream()
                         .map(ImageUtil::appendUri).collect(Collectors.toList()))
                 .build();
