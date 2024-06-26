af98eea4f069057718d5b58559840d03923282d5
hann <wjdgks1233@gmail.com>
2024-06-27 07:01:24 +0900

    [Refactor/Security/#48] add reissue api

--- src/main/java/com/bandit/security/controller/TokenApiController.java
+++ src/main/java/com/bandit/security/controller/TokenApiController.java
@@ -9,11 +9,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 @Tag(name = "Token API", description = "토큰 API")
 @ApiResponse(responseCode = "2000", description = "성공")
 @RequiredArgsConstructor
@@ -22,13 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TokenApiController {
     private final TokenService tokenService;
 
-    @Operation(summary = "로그인(토큰 생성)", description = "카카오이메일을 통해 로그인합니다.")
+    @Operation(summary = "토큰 재발행", description = "리프레시 토큰을 통해 토큰을 재발행 받습니다.")
     @ApiErrorCodeExample({
             ErrorStatus._INTERNAL_SERVER_ERROR,
             ErrorStatus.MEMBER_NOT_FOUND
     })
-    @GetMapping("/login")
-    public ApiResponseDto<JwtToken> login(@RequestParam String kakaoEmail) {
-        return ApiResponseDto.onSuccess(tokenService.login(kakaoEmail));
+    @PostMapping("/reissue")
+    public ApiResponseDto<JwtToken> issueToken(@RequestParam String refresh) {
+        return ApiResponseDto.onSuccess(tokenService.issueTokens(refresh));
     }
 }
