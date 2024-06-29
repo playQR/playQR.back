de0049aed13e329d3c4b8c8a715bdee7624640e2
k168ww <k168ww@gmail.com>
2024-06-29 09:26:22 +0900

    #35 dto 추가

--- src/main/java/com/rockoon/domain/ticket/dto/guest/GuestResponse.java
+++ src/main/java/com/rockoon/domain/ticket/dto/guest/GuestResponse.java
@@ -1,4 +1,36 @@
 package com.rockoon.domain.ticket.dto.guest;
 
+import com.rockoon.domain.member.dto.MemberResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GuestResponse {
+    private Long guestId;
+    private String guestname;
+    private MemberResponse writer;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GuestSummaryDto {
+        private Long guestId;
+        private String guestname;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GuestListDto {
+        private List<GuestSummaryDto> guestList;
+    }
 }
