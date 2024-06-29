25d36ecda3220147f1602ba947d2410d104e2cdf
k168ww <k168ww@gmail.com>
2024-06-29 09:26:22 +0900

    #35 dto 추가

--- src/main/java/com/rockoon/domain/ticket/dto/guest/GuestRequest.java
+++ src/main/java/com/rockoon/domain/ticket/dto/guest/GuestRequest.java
@@ -1,4 +1,23 @@
 package com.rockoon.domain.ticket.dto.guest;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GuestRequest {
+    private Long promotionId;
+    private String guestname;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GuestModifyDto {
+        private String guestname;
+    }
 }
--- src/main/java/com/rockoon/domain/ticket/dto/ticket/TicketRequest.java
+++ src/main/java/com/rockoon/domain/ticket/dto/ticket/TicketRequest.java
@@ -1,4 +1,27 @@
 package com.rockoon.domain.ticket.dto.ticket;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
 public class TicketRequest {
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TicketRegisterDto {
+        private Long guestId;
+        private Date dueDate;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TicketModifyDto {
+        private Date dueDate;
+    }
 }
