f52dad3c2a2e67f073c9ddca72df0ecbfacab967
hann <wjdgks1233@gmail.com>
2024-06-30 23:14:38 +0900

    [Feat/Message/#50] edit kakao service for parsing

--- src/main/java/com/bandit/global/service/KakaoService.java
+++ src/main/java/com/bandit/global/service/KakaoService.java
@@ -5,6 +5,8 @@ import com.bandit.global.dto.KakaoFriendDto;
 import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -26,22 +28,28 @@ public class KakaoService extends HttpCallService {
 
         ResponseEntity<String> response = httpRequest(GET_FRIENDS_LIST_URL, HttpMethod.GET, entity);
         List<KakaoFriendDto> list = new ArrayList<>();
-        JSONObject jsonObject = new JSONObject(Integer.parseInt(response.getBody()));
-        JSONArray elements = (JSONArray) jsonObject.get("elements");
 
-        for (Object element : elements) {
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(response.getBody());
+            JSONArray elements = (JSONArray) jsonObject.get("elements");
 
-            KakaoFriendDto dto = new KakaoFriendDto();
-            JSONObject obj = (JSONObject) element;
-            log.info("obj = {}", obj.get("profile_nickname").toString());
+            for (Object element : elements) {
+                KakaoFriendDto dto = new KakaoFriendDto();
+                JSONObject obj = (JSONObject) element;
+                log.info("obj = {}", obj.get("profile_nickname").toString());
 
-            dto.setId(obj.get("id").toString());
-            dto.setUuid(obj.get("uuid").toString());
-            dto.setFavorite((boolean) obj.get("favorite"));
-            dto.setProfile_nickname(obj.get("profile_nickname").toString());
-            dto.setProfile_thumbnail_image(obj.get("profile_thumbnail_image").toString());
+                dto.setId(obj.get("id").toString());
+                dto.setUuid(obj.get("uuid").toString());
+                dto.setFavorite((boolean) obj.get("favorite"));
+                dto.setProfile_nickname(obj.get("profile_nickname").toString());
+                dto.setProfile_thumbnail_image(obj.get("profile_thumbnail_image").toString());
 
-            list.add(dto);
+                list.add(dto);
+            }
+
+        } catch (ParseException e) {
+            log.error("JSON parsing error", e);
         }
 
         return list;
