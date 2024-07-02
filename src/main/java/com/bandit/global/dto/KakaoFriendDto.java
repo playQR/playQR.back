e0f02973d62e66f8cab792f6d60a65b4927baba3
hann <wjdgks1233@gmail.com>
2024-07-02 15:35:13 +0900

    [Feat/Message/#50] create sendMessage method in KakaoService

--- src/main/java/com/bandit/global/dto/KakaoFriendDto.java
+++ src/main/java/com/bandit/global/dto/KakaoFriendDto.java
@@ -1,8 +1,14 @@
 package com.bandit.global.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class KakaoFriendDto {
     private String id;
     private String uuid;
---
+++ src/main/java/com/bandit/global/dto/KakaoMessageDto.java
@@ -0,0 +1,18 @@
+package com.bandit.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class KakaoMessageDto {
+    private String objType;
+    private String text;
+    private String webUrl;
+    private String mobileUrl;
+    private String btnTitle;
+}
