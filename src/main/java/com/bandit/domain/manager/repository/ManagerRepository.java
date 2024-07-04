7c5ae0ab7a9c463ae35b80f1603e63496ff62b0c
k168ww <k168ww@gmail.com>
2024-07-04 20:19:11 +0900

    #54 add QueryService code

--- src/main/java/com/bandit/domain/manager/repository/ManagerRepository.java
+++ src/main/java/com/bandit/domain/manager/repository/ManagerRepository.java
@@ -5,9 +5,13 @@ import com.bandit.domain.manager.entity.Manager;
 import com.bandit.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
+
     Optional<Manager> findByPromotionAndMember(Promotion promotion, Member member);
+
+    List<Manager> findByPromotion(Promotion promotion);
 }
 
