d292ed330f53ab1d3c4f8dd9a7129d478d8a6f63
k168ww <k168ww@gmail.com>
2024-07-08 12:28:11 +0900

    #54 add converter code

--- src/main/java/com/bandit/domain/manager/converter/ManagerConverter.java
+++ src/main/java/com/bandit/domain/manager/converter/ManagerConverter.java
@@ -1,4 +1,38 @@
 package com.bandit.domain.manager.converter;
 
+import com.bandit.domain.member.converter.MemberConverter;
+import com.bandit.domain.manager.dto.ManagerResponse.ManagerListDto;
+import com.bandit.domain.manager.dto.ManagerResponse.ManagerViewDto;
+import com.bandit.domain.manager.entity.Manager;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ManagerConverter {
+
+    public static ManagerViewDto toViewDto(Manager manager) {
+        return ManagerViewDto.builder()
+                .managerId(manager.getId())
+                .member(MemberConverter.toResponse(manager.getMember()))
+                .build();
+    }
+
+    public static ManagerListDto toListDto(List<Manager> managerList) {
+        List<ManagerViewDto> collect = managerList.stream()
+                .map(ManagerConverter::toViewDto)
+                .collect(Collectors.toList());
+        return ManagerListDto.builder()
+                .managerList(collect)
+                .build();
+    }
+
+    public static ManagerListDto toListDto(Page<Manager> managerPage) {
+        List<ManagerViewDto> collect = managerPage.getContent().stream()
+                .map(ManagerConverter::toViewDto)
+                .collect(Collectors.toList());
+        return ManagerListDto.builder()
+                .managerList(collect)
+                .build();
+    }
 }
