623450015c01f8d39353098bf26e41364fabdd53
hann <wjdgks1233@gmail.com>
2024-07-09 07:18:50 +0900

    [Feat/Like/#58] complete command service logic

--- src/main/java/com/bandit/domain/like/repository/LikePromotionRepository.java
+++ src/main/java/com/bandit/domain/like/repository/LikePromotionRepository.java
@@ -1,7 +1,9 @@
 package com.bandit.domain.like.repository;
 
 import com.bandit.domain.like.entity.LikePromotion;
+import com.bandit.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikePromotionRepository extends JpaRepository<LikePromotion, Long> {
+    void deleteByPromotionIdAndMember(Long promotionId, Member member);
 }
--- src/main/java/com/bandit/domain/like/service/like_promotion/LikePromotionCommandServiceImpl.java
+++ src/main/java/com/bandit/domain/like/service/like_promotion/LikePromotionCommandServiceImpl.java
@@ -1,4 +1,32 @@
 package com.bandit.domain.like.service.like_promotion;
 
-public class LikePromotionCommandServiceImpl {
+import com.bandit.domain.board.entity.Promotion;
+import com.bandit.domain.board.repository.PromotionRepository;
+import com.bandit.domain.like.entity.LikePromotion;
+import com.bandit.domain.like.repository.LikePromotionRepository;
+import com.bandit.domain.member.entity.Member;
+import com.bandit.presentation.payload.code.ErrorStatus;
+import com.bandit.presentation.payload.exception.PromotionHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class LikePromotionCommandServiceImpl implements LikePromotionCommandService{
+    private final LikePromotionRepository likePromotionRepository;
+    private final PromotionRepository promotionRepository;
+    @Override
+    public Long likePromotion(Long promotionId, Member member) {
+        Promotion promotion = promotionRepository.findById(promotionId)
+                .orElseThrow(() -> new PromotionHandler(ErrorStatus.PROMOTION_NOT_FOUND));
+        LikePromotion save = likePromotionRepository.save(LikePromotion.of(promotion, member));
+        return save.getId();
+    }
+
+    @Override
+    public void unlikePromotion(Long promotionId, Member member) {
+        likePromotionRepository.deleteByPromotionIdAndMember(promotionId, member);
+    }
 }
