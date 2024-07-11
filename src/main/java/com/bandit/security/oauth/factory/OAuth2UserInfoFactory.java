044672937a0254cfcf042459f32e914c6b7c4f5e
hann <wjdgks1233@gmail.com>
2024-07-11 12:46:40 +0900

    [Refactor/Nickname/#65] get profile_nickname from kakao and put it at member's name

--- src/main/java/com/bandit/security/oauth/factory/OAuth2UserInfoFactory.java
+++ src/main/java/com/bandit/security/oauth/factory/OAuth2UserInfoFactory.java
@@ -1,12 +1,11 @@
 package com.bandit.security.oauth.factory;
 
 import com.bandit.security.oauth.dto.KakaoOAuth2User;
-import com.bandit.security.oauth.dto.OAuth2UserInfo;
 
 import java.util.Map;
 
 public class OAuth2UserInfoFactory {
-    public static OAuth2UserInfo getOAuth2UserInfo(Map<String, Object> attributes) {
+    public static KakaoOAuth2User getOAuth2UserInfo(Map<String, Object> attributes) {
         return new KakaoOAuth2User(attributes);
     }
 
