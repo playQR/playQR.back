fbd3f9d21d1b096fe2c326cc7c9d2bd20fc3892f
hann <wjdgks1233@gmail.com>
2024-07-15 10:46:21 +0900

    [Feat/Api/#85] add api of my favorite promotions

--- src/main/java/com/bandit/domain/like/repository/LikePromotionRepository.java
+++ src/main/java/com/bandit/domain/like/repository/LikePromotionRepository.java
@@ -1,10 +1,11 @@
 package com.bandit.domain.like.repository;
 
 import com.bandit.domain.like.entity.LikePromotion;
+import com.bandit.domain.like.repository.querydsl.LikePromotionQueryRepository;
 import com.bandit.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikePromotionRepository extends JpaRepository<LikePromotion, Long> {
+public interface LikePromotionRepository extends JpaRepository<LikePromotion, Long>, LikePromotionQueryRepository {
     void deleteByPromotionIdAndMember(Long promotionId, Member member);
 
     boolean existsByPromotionIdAndMember(Long promotionId, Member member);
